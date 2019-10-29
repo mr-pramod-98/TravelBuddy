37f8e8eb1292aec8f76be77fafefd0705cbd74af
mr-pramod-98 <43513968+mr-pramod-98@users.noreply.github.com>
2019-10-29 11:44:33 +0530

    added Meta class

--- TravelBuddy/travello/models.py
+++ TravelBuddy/travello/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 
 
 # Create your models here.
 class Packages(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Packages"
+
     location_id = models.CharField(max_length=15, primary_key=True)
     location_name = models.CharField(max_length=50)
     sub_title = models.TextField()
@@ -16,36 +21,67 @@ class Packages(models.Model):
 
 
 class DestinationDetails(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Destination details"
+
     place_id = models.CharField(max_length=25, primary_key=True)
     location_id = models.ForeignKey(Packages, on_delete=models.CASCADE)
     place_name = models.CharField(max_length=50)
-    visiting_on = models.IntegerField(default=0)
+    visiting_on = models.PositiveIntegerField(validators=[MinValueValidator(1)])
     place_image = models.ImageField(upload_to='places')
 
 
 class PackagesGuide(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Package guide"
+
     guide_id = models.CharField(max_length=25, primary_key=True)
     location_id = models.ForeignKey(Packages, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='guides', default="../static/travello/images/main2.jpg")
     guide_name = models.CharField(max_length=20)
     age = models.IntegerField()
     experience = models.IntegerField()
 
 
 class PackagesVehicle(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Package vehicles"
+
     reg_number = models.CharField(max_length=13, primary_key=True)
     location_id = models.ForeignKey(Packages, on_delete=models.CASCADE)
     vehicle_name = models.CharField(max_length=50)
     capacity = models.IntegerField()
+    vehicle_image_1 = models.ImageField(upload_to='vehicles', default=None)
+    vehicle_image_2 = models.ImageField(upload_to='vehicles', default=None)
+    vehicle_image_3 = models.ImageField(upload_to='vehicles', default=None)
+    vehicle_image_4 = models.ImageField(upload_to='vehicles', default=None)
 
 
 class PackagesHotel(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Package hotels"
+
     hotel_id = models.CharField(max_length=35, primary_key=True)
     location_id = models.ForeignKey(Packages, on_delete=models.CASCADE)
     hotel_name = models.CharField(max_length=20)
     room_capacity = models.IntegerField()
+    hotel_image_1 = models.ImageField(upload_to='hotels', default=None)
+    hotel_image_2 = models.ImageField(upload_to='hotels', default=None)
+    hotel_image_3 = models.ImageField(upload_to='hotels', default=None)
+    hotel_image_4 = models.ImageField(upload_to='hotels', default=None)
+    hotel_image_5 = models.ImageField(upload_to='hotels', default=None)
+    hotel_image_6 = models.ImageField(upload_to='hotels', default=None)
 
 
 class PackagesBookings(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Booking details"
+
     booking_id = models.CharField(max_length=150, primary_key=True)
     location_id = models.ForeignKey(Packages, on_delete=models.CASCADE)
     username = models.ForeignKey(User, max_length=150, on_delete=models.CASCADE)
