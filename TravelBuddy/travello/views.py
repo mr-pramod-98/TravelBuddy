64ffd5e43da2ca9ce5a65472f4c9fa158588cecd
mr-pramod-98 <43513968+mr-pramod-98@users.noreply.github.com>
2019-12-09 11:39:38 +0530

    checking for user authentication

--- TravelBuddy/travello/views.py
+++ TravelBuddy/travello/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 import mysql.connector
 from .models import Packages, DestinationDetails, PackagesGuide, PackagesHotel, VehicleAndGeneralDetails, HotelImages
 from datetime import datetime
@@ -140,151 +140,199 @@ class FindVacancies:
 # "travello" METHOD IS CALLED WHET THE USER LOGIN'S SUCCESSFULLY AND EVERY-TIME USER REQUESTS FOR "travello" PAGE
 def travello(request, username):
 
-    # FETCHING ALL RECORDS( i.e, DESTINATIONS) FOR "Packages" TABLE
-    dests = Packages.objects.all()
+    # CHECK IS USER AUTHENTICATED(i.e, is the user logged in or not)
+    # EXECUTE "if block" IF USER AUTHENTICATED
+    if request.user.is_authenticated:
 
-    # CALCULATING THE VACANCIES IN EVERY PACKAGE
-    finder = FindVacancies()
-    vacancies = finder.calculate_for_all(dests)
+        # FETCHING ALL RECORDS( i.e, DESTINATIONS) FOR "Packages" TABLE
+        dests = Packages.objects.all()
 
-    # CONNECTING TO DATABASE TO FETCH THE COUNT OF AVAILABLE OFFERS ON PACKAGES
-    my_db = mysql.connector.connect(host='localhost', user='root', passwd='password', database='TravelBuddy')
-    my_cur = my_db.cursor()
+        # CALCULATING THE VACANCIES IN EVERY PACKAGE
+        finder = FindVacancies()
+        vacancies = finder.calculate_for_all(dests)
 
-    # QUERY TO FETCH COUNT OF AVAILABLE OFFERS ON PACKAGES
-    query = "select count(offer) " \
-            "from travello_packages " \
-            "where offer = %s"
+        # CONNECTING TO DATABASE TO FETCH THE COUNT OF AVAILABLE OFFERS ON PACKAGES
+        my_db = mysql.connector.connect(host='localhost', user='root', passwd='password', database='TravelBuddy')
+        my_cur = my_db.cursor()
 
-    my_cur.execute(query, (1,))
+        # QUERY TO FETCH COUNT OF AVAILABLE OFFERS ON PACKAGES
+        query = "select count(offer) " \
+                "from travello_packages " \
+                "where offer = %s"
 
-    # FETCHING THE VALUE FROM LIST OF TUPLES
-    count_of_offers = my_cur.fetchall()[0][0]
+        my_cur.execute(query, (1,))
 
-    # RETURNING THE REQUESTED PAGE( i.e, travello.html)
-    # "dests" AND "vacancies" ARE ZIPPED SO THAT THEY CAN BE ITERATED OVER FOR LOOP SIMULTANEOUSLY
-    return render(request, 'travello.html', {'dests_vacancies': zip(dests, vacancies), 'offers': count_of_offers})
+        # FETCHING THE VALUE FROM LIST OF TUPLES
+        count_of_offers = my_cur.fetchall()[0][0]
+
+        # RETURNING THE REQUESTED PAGE( i.e, travello.html)
+        # "dests" AND "vacancies" ARE ZIPPED SO THAT THEY CAN BE ITERATED OVER FOR LOOP SIMULTANEOUSLY
+        return render(request, 'travello.html', {'dests_vacancies': zip(dests, vacancies), 'offers': count_of_offers})
+
+    # EXECUTE "else block" IF USER IS NOT AUTHENTICATED
+    else:
+
+        # REDIRECTING THE USER TO "login page"
+        return redirect('login')
 
 
 # "destination_details" METHOD IS CALLED WHET THE USER REQUESTS FOR "destinations" PAGE
 def destination_details(request, username, destination_id):
 
-    dest = Packages.objects.get(location_id=destination_id)
+    # CHECK IS USER AUTHENTICATED(i.e, is the user logged in or not)
+    # EXECUTE "if block" IF USER AUTHENTICATED
+    if request.user.is_authenticated:
 
-    # FETCHING ALL RELATED RECORDS OF A PARTICULAR LOCATION FROM "DestinationDetails" TABLE AND
-    # ORDERING THEM BASED ON "visiting_on" FIELD
-    dests_local = DestinationDetails.objects.all().filter(location_id=destination_id).order_by('visiting_on')
+        dest = Packages.objects.get(location_id=destination_id)
 
-    # FETCHING ALL RELATED RECORDS OF A PARTICULAR LOCATION FROM "PackagesGuide" TABLE
-    guides = PackagesGuide.objects.all().filter(location_id=destination_id)
+        # FETCHING ALL RELATED RECORDS OF A PARTICULAR LOCATION FROM "DestinationDetails" TABLE AND
+        # ORDERING THEM BASED ON "visiting_on" FIELD
+        dests_local = DestinationDetails.objects.all().filter(location_id=destination_id).order_by('visiting_on')
 
-    # FETCHING ALL RELATED RECORDS OF A PARTICULAR LOCATION FROM "PackagesHotel" TABLE
-    hotels = PackagesHotel.objects.get(location_id=destination_id)
+        # FETCHING ALL RELATED RECORDS OF A PARTICULAR LOCATION FROM "PackagesGuide" TABLE
+        guides = PackagesGuide.objects.all().filter(location_id=destination_id)
 
-    # FETCHING ALL RELATED RECORDS OF A PARTICULAR HOTEL FROM "PackagesHotel" TABLE
-    hotel_images = HotelImages.objects.all().filter(hotel_id=hotels.hotel_id)
+        # FETCHING ALL RELATED RECORDS OF A PARTICULAR LOCATION FROM "PackagesHotel" TABLE
+        hotels = PackagesHotel.objects.get(location_id=destination_id)
 
-    # FETCHING ALL RELATED RECORDS OF A PARTICULAR LOCATION FROM "PackagesHotel" TABLE
-    general_details = VehicleAndGeneralDetails.objects.get(location_id=destination_id)
+        # FETCHING ALL RELATED RECORDS OF A PARTICULAR HOTEL FROM "PackagesHotel" TABLE
+        hotel_images = HotelImages.objects.all().filter(hotel_id=hotels.hotel_id)
 
-    # CALCULATING THE VACANCY FOR A PARTICULAR LOCATION
-    finder = FindVacancies()
-    vacancy = finder.calculate(destination_id)
+        # FETCHING ALL RELATED RECORDS OF A PARTICULAR LOCATION FROM "PackagesHotel" TABLE
+        general_details = VehicleAndGeneralDetails.objects.get(location_id=destination_id)
 
-    # CONNECTING TO DATABASE TO FETCH THE LAST DATE TO BOOK THE SELECTED PACKAGE
-    my_db = mysql.connector.connect(host='localhost', user='root', passwd='password', database='TravelBuddy')
-    my_cur = my_db.cursor()
+        # CALCULATING THE VACANCY FOR A PARTICULAR LOCATION
+        finder = FindVacancies()
+        vacancy = finder.calculate(destination_id)
 
-    # QUERY TO FETCH THE LAST DATE TO BOOK A PARTICULAR PACKAGE --> STARTS
-    query = "select date " \
-            "from travello_packages " \
-            "where location_id = %s"
-
-    my_cur.execute(query, (destination_id,))
-
-    last_date = str(my_cur.fetchall()[0][0])
-    # QUERY TO FETCH THE LAST DATE TO BOOK A PARTICULAR PACKAGE --> ENDS
-
-    # FETCHING TODAY'S DATE
-    today = str(datetime.now()).split(" ")[0]
-
-    # IF TODAY'S DATE IS LESS THEN LAST BOOKING DATE THEN SET "can_be_booked" TO True ELSE False
-    if today <= last_date:
-        can_be_booked = 1
-    else:
-        can_be_booked = 0
-
-    # RETURNING THE REQUESTED PAGE( i.e, destinations.html)
-    return render(request, 'destinations.html', {'dest': dest, 'dests_local': dests_local, 'guides': guides,
-                                                 'hotels': hotels, 'destination_id': destination_id, 'vacancy': vacancy,
-                                                 'can_be_booked': can_be_booked, 'general_details': general_details,
-                                                 'hotel_images': hotel_images})
-
-
-# "bookings" METHOD IS CALLED WHET THE USER CLICK'S ON "Book Now :)" BUTTON
-def bookings(request, username, destination_id):
-
-    # IF THE REQUEST METHOD IS "POST" THEN EXECUTE if BLOCK
-    if request.method == "POST":
-
-        # READING NUMBER OF SEATS REQUESTED TO BE BOOKED
-        seat_count = request.POST["seat_count"]
-
-        # CONNECTING TO DATABASE TO FETCH THE BASIC PRICE OF THE SELECTED PACKAGE
+        # CONNECTING TO DATABASE TO FETCH THE LAST DATE TO BOOK THE SELECTED PACKAGE
         my_db = mysql.connector.connect(host='localhost', user='root', passwd='password', database='TravelBuddy')
         my_cur = my_db.cursor()
 
-        # QUERY TO FETCH PRICE OF THE SELECTED PACKAGE(i.e, price per person)
-        query = "select price " \
+        # QUERY TO FETCH THE LAST DATE TO BOOK A PARTICULAR PACKAGE --> STARTS
+        query = "select date " \
                 "from travello_packages " \
                 "where location_id = %s"
 
         my_cur.execute(query, (destination_id,))
 
-        # FETCHING THE VALUE FROM LIST OF TUPLES
-        price_per_person = my_cur.fetchall()[0][0]
+        last_date = str(my_cur.fetchall()[0][0])
+        # QUERY TO FETCH THE LAST DATE TO BOOK A PARTICULAR PACKAGE --> ENDS
 
-        # CALCULATING THE TOTAL COST BY MULTIPLYING "NUMBER OF SEATS" AND "PRICE PER PERSON"
-        total_cost = int(seat_count) * int(price_per_person)
+        # FETCHING TODAY'S DATE
+        today = str(datetime.now()).split(" ")[0]
 
-        # RETURNING THE REQUESTED PAGE( i.e, bookings.html)
-        return render(request, 'bookings.html', {'seat_count': seat_count, 'total_cost': total_cost,
-                                                 'destination_id': destination_id})
+        # IF TODAY'S DATE IS LESS THEN LAST BOOKING DATE THEN SET "can_be_booked" TO True ELSE False
+        if today <= last_date:
+            can_be_booked = 1
+        else:
+            can_be_booked = 0
+
+        # RETURNING THE REQUESTED PAGE( i.e, destinations.html)
+        return render(request, 'destinations.html', {'dest': dest, 'dests_local': dests_local, 'guides': guides,
+                                                     'hotels': hotels, 'destination_id': destination_id,
+                                                     'vacancy': vacancy, 'can_be_booked': can_be_booked,
+                                                     'general_details': general_details, 'hotel_images': hotel_images})
+
+    # EXECUTE "else block" IF USER IS NOT AUTHENTICATED
+    else:
+
+        # REDIRECTING THE USER TO "login page"
+        return redirect('login')
+
+
+# "bookings" METHOD IS CALLED WHET THE USER CLICK'S ON "Book Now :)" BUTTON
+def bookings(request, username, destination_id):
+
+    # CHECK IS USER AUTHENTICATED(i.e, is the user logged in or not)
+    # EXECUTE "if block" IF USER AUTHENTICATED
+    if request.user.is_authenticated:
+
+        # IF THE REQUEST METHOD IS "POST" THEN EXECUTE if BLOCK
+        if request.method == "POST":
+
+            # READING NUMBER OF SEATS REQUESTED TO BE BOOKED
+            seat_count = request.POST["seat_count"]
+
+            # CONNECTING TO DATABASE TO FETCH THE BASIC PRICE OF THE SELECTED PACKAGE
+            my_db = mysql.connector.connect(host='localhost', user='root', passwd='password', database='TravelBuddy')
+            my_cur = my_db.cursor()
+
+            # QUERY TO FETCH PRICE OF THE SELECTED PACKAGE(i.e, price per person)
+            query = "select price " \
+                    "from travello_packages " \
+                    "where location_id = %s"
+
+            my_cur.execute(query, (destination_id,))
+
+            # FETCHING THE VALUE FROM LIST OF TUPLES
+            price_per_person = my_cur.fetchall()[0][0]
+
+            # CALCULATING THE TOTAL COST BY MULTIPLYING "NUMBER OF SEATS" AND "PRICE PER PERSON"
+            total_cost = int(seat_count) * int(price_per_person)
+
+            # RETURNING THE REQUESTED PAGE( i.e, bookings.html)
+            return render(request, 'bookings.html', {'seat_count': seat_count, 'total_cost': total_cost,
+                                                     'destination_id': destination_id})
+
+        else:
+            # REDIRECTING THE USER BACK TO THE CORRESPONDING "destination page"
+            return redirect('/travello/TravelBuddy/' + username + '/' + destination_id)
+
+    # EXECUTE "else block" IF USER IS NOT AUTHENTICATED
+    else:
+
+        # REDIRECTING THE USER TO "login page"
+        return redirect('login')
 
 
 def bookings_confirmed(request, username, destination_id):
 
-    # IF THE REQUEST METHOD IS "POST" THEN EXECUTE if BLOCK
-    if request.method == "POST":
+    # CHECK IS USER AUTHENTICATED(i.e, is the user logged in or not)
+    # EXECUTE "if block" IF USER AUTHENTICATED
+    if request.user.is_authenticated:
 
-        # EXTRACTING "14" FROM "Number of seats: 14"
-        seat_count = request.POST["seat_count"].split(" ")[-1]
+        # IF THE REQUEST METHOD IS "POST" THEN EXECUTE if BLOCK
+        if request.method == "POST":
 
-        # EXTRACTING "700" FROM "Amount: $700"
-        amount = request.POST["amount"].split(" ")[-1][1:]
+            # EXTRACTING "14" FROM "Number of seats: 14"
+            seat_count = request.POST["seat_count"].split(" ")[-1]
 
-        email = request.POST["email"]
+            # EXTRACTING "700" FROM "Amount: $700"
+            amount = request.POST["amount"].split(" ")[-1][1:]
 
-        # GENERATING "booking_id" BY CONCATENATING "username" AND THE "DATE AND TIME" OF BOOKING
-        booking_id = username + str(datetime.now())
+            email = request.POST["email"]
 
-        # CONNECTING TO DATABASE TO STORE THE BOOKING DETAILS
-        my_db = mysql.connector.connect(host='localhost', user='root', passwd='password', database='TravelBuddy')
-        my_cur = my_db.cursor()
+            # GENERATING "booking_id" BY CONCATENATING "username" AND THE "DATE AND TIME" OF BOOKING
+            booking_id = username + str(datetime.now())
 
-        # QUERY TO INSERT THE RECORD INTO "travello_packagesbookings" TABLE
-        query = "INSERT INTO travello_packagesbookings" \
-                "(booking_id, number_of_seats, total_amount, location_id_id, username_id) " \
-                "VALUES(%s, %s, %s, %s, %s)"
+            # CONNECTING TO DATABASE TO STORE THE BOOKING DETAILS
+            my_db = mysql.connector.connect(host='localhost', user='root', passwd='password', database='TravelBuddy')
+            my_cur = my_db.cursor()
 
-        values = (booking_id, seat_count, amount, destination_id, username)
+            # QUERY TO INSERT THE RECORD INTO "travello_packagesbookings" TABLE
+            query = "INSERT INTO travello_packagesbookings" \
+                    "(booking_id, number_of_seats, total_amount, location_id_id, username_id) " \
+                    "VALUES(%s, %s, %s, %s, %s)"
 
-        my_cur.execute(query, values)
-        my_db.commit()
+            values = (booking_id, seat_count, amount, destination_id, username)
 
-        # SEND CONFIRMATION MAIL
-        send_mail = SendConfirmationMail()
-        send_mail.confirmation_mail(username, email, booking_id, seat_count, amount)
+            my_cur.execute(query, values)
+            my_db.commit()
 
-        # RETURNING THE CONFORMATION MESSAGE TO THE USER
-        return HttpResponse("Booking confirmed check your mail id (" + email + ")for more details")
+            # SEND CONFIRMATION MAIL
+            send_mail = SendConfirmationMail()
+            send_mail.confirmation_mail(username, email, booking_id, seat_count, amount)
+
+            # RETURNING THE CONFORMATION MESSAGE TO THE USER
+            return HttpResponse("Booking confirmed check your mail id (" + email + ")for more details")
+
+        else:
+            # REDIRECTING THE USER BACK TO THE CORRESPONDING "destination page"
+            return redirect('/travello/TravelBuddy/' + username + '/' + destination_id)
+
+    # EXECUTE "else block" IF USER IS NOT AUTHENTICATED
+    else:
+
+        # REDIRECTING THE USER TO "login page"
+        return redirect('login')
